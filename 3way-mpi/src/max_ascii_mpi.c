1b17588aed80affbfc8bd90f0a05c05be31e4d00
John Spexarth <jspex@ksu.edu>
2026-04-26 23:26:22 -0500

    mpi work

--- 3way-mpi/src/max_ascii_mpi.c
+++ 3way-mpi/src/max_ascii_mpi.c
@@ -2,7 +2,6 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,137 +9,149 @@
 #include <unistd.h>
 #include "file_io.h"
 
-typedef struct {
-    int thread_id;
-    size_t start;
-    size_t end;
-    int *max_values;
-    file_string_array_t *fsa;
-} thread_data_t;
-
-void *process_chunk(void *args) {
-    thread_data_t *t_data = (thread_data_t *)args;
-    int charOrd, maxOrd;
-
-    // Loop through each line
-    for (size_t i = t_data->start; i < t_data->end; i++) {
-        maxOrd = -1;
-        // Loop through the string until we hit the null terminator
-        for (int j = 0; t_data->fsa->line_array[i][j] != '\0'; j++) {
-            // Get the ordinal for this character
-            charOrd = (int)t_data->fsa->line_array[i][j];
-    
-            // Skip non-ASCII characters
-            if (charOrd < 0 || charOrd > 127) {
-                continue;
-            }
-
-            // Check if this character is the largest we've seen
-            if (charOrd > maxOrd) {
-                maxOrd = charOrd;
-            }
-        }
-
-        // Shouldn't need a mutex since each thread has an exclusive chunk
-        t_data->max_values[i] = maxOrd;
-    }
-
-    return NULL;
-}
+#define BATCH_SIZE 10000
 
 int main(int argc, char *argv[])
 {
-    MPI_Init(NULL, NULL);
+    MPI_Init(&argc, &argv);
 
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); // this process ID
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    char line_array_rcv[BUFFER_SIZE]
+    file_string_array_t fsa;
+    size_t total_lines = 0;
 
     if ( rank == 0 )
     {
         const char *filename = "/homes/eyv/cis520/wiki_dump.txt";
-
-        file_string_array_t fsa;
         
         if (!read_text_file((char*)filename, &fsa)) {
-            fprintf(stderr, "Failed to read file\n");
-            return 1;
+            fprintf(stderr, "Rank 0: Failed to read file %s\n", filename);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        total_lines = fsa.count;
+        printf("Rank 0: Successfully read %zu lines from wiki_dump.txt\n", total_lines);
+    } else {
+        if (!create_fsa(&fsa)) {
+            fprintf(stderr, "Rank %d: Failed to create fsa\n", rank);
+            MPI_Abort(MPI_COMM_WORLD, 1);
         }
     }
 
-    // Calculate which lines this node is responsible for
-    size_t lines_per_node = fsa.count / size;
-    size_t remainder = fsa.count % size;
+    MPI_Bcast(&total_lines, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
 
-    for ( int i = 0; i < lines_per_node; i++ )
-    {
-        MPI_Scatter(fsa->line_array)
-    }
-    
-
-    
-
-    size_t node_start = 0;
-    for (int n = 0; n < my_node; n++) {
-        node_start += lines_per_node + (n < (int)remainder ? 1 : 0);
-    }
-    size_t node_end = node_start + lines_per_node + (my_node < (int)remainder ? 1 : 0);
-
-    size_t my_line_count = node_end - node_start;
-    int *max_values = calloc(my_line_count, sizeof(int));
-    if (!max_values) {
+    if (total_lines == 0) {
+        if (rank == 0) fprintf(stderr, "No lines in file!\n");
         destroy_fsa(&fsa);
+        MPI_Finalize();
         return 1;
     }
 
-    pthread_t *threads = malloc(num_threads * sizeof(pthread_t));
-    if (!threads) {
+    size_t lines_per_rank = total_lines / size;
+    size_t remainder = total_lines % size;
+
+    size_t my_start = 0;
+    for (int r = 0; r < rank; r++) {
+        my_start += lines_per_rank + (r < (int)remainder ? 1 : 0);
+    }
+    size_t my_count = lines_per_rank + (rank < (int)remainder ? 1 : 0);
+
+    // Distribute lines
+    if (rank == 0) {
+        for (int r = 1; r < size; r++) {
+            size_t start_r = 0;
+            for (int i = 0; i < r; i++) {
+                start_r += lines_per_rank + (i < (int)remainder ? 1 : 0);
+            }
+            size_t count_r = lines_per_rank + (r < (int)remainder ? 1 : 0);
+
+            for (size_t i = 0; i < count_r; i += BATCH_SIZE) {
+                size_t batch = (i + BATCH_SIZE < count_r) ? BATCH_SIZE : (count_r - i);
+                for (size_t j = 0; j < batch; j++) {
+                    char *line = fsa.line_array[start_r + i + j];
+                    size_t len = strlen(line) + 1;
+                    MPI_Send(&len, 1, MPI_UNSIGNED_LONG, r, 0, MPI_COMM_WORLD);
+                    MPI_Send(line, len, MPI_CHAR, r, 1, MPI_COMM_WORLD);
+                }
+            }
+        }
+    } else {
+        char *recv_buffer = malloc(BUFFER_SIZE);
+        if (recv_buffer == NULL) {
+            fprintf(stderr, "Rank %d: malloc failed\n", rank);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+        for (size_t i = 0; i < my_count; i++) {
+            size_t len;
+            MPI_Recv(&len, 1, MPI_UNSIGNED_LONG, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(recv_buffer, len, MPI_CHAR, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            fsa_add_line(&fsa, recv_buffer);
+        }
+        free(recv_buffer);
+    }
+
+    // Now every rank has its local lines in fsa
+
+    // Process lines
+    int *local_max = calloc(my_count, sizeof(int));
+    if (local_max == NULL) {
+        fprintf(stderr, "Rank %d: calloc failed for local_max\n", rank);
         destroy_fsa(&fsa);
-        free(max_values);
-        return 1;
-    }
-    thread_data_t *tdata = malloc(num_threads * sizeof(thread_data_t));
-    if (!tdata) {
-        destroy_fsa(&fsa);
-        free(max_values);
-        free(threads);
-        return 1;
+        MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    size_t lines_per_thread = my_line_count / num_threads;
-    size_t thread_remainder = my_line_count % num_threads;
-    size_t start = 0;
+    for (size_t i = 0; i < my_count; i++) {
+        int maxOrd = -1;
+        char *line = fsa.line_array[i];
 
-    for (int i = 0; i < num_threads; i++) {
-        size_t extra = (i < (int)thread_remainder) ? 1 : 0;
-        tdata[i].thread_id = i;
-        tdata[i].start = node_start + start;
-        tdata[i].end = node_start + start + lines_per_thread + extra;
-        tdata[i].max_values = max_values;
-        tdata[i].fsa = &fsa;
-        
-        pthread_create(&threads[i], NULL, process_chunk, &tdata[i]);
-        start += lines_per_thread + extra;
+        for (int j = 0; line[j] != '\0'; j++) {
+            int charOrd = (int)line[j];
+            if (charOrd >= 0 && charOrd <= 127 && charOrd > maxOrd) {
+                maxOrd = charOrd;
+            }
+        }
+        local_max[i] = maxOrd;
     }
 
-    for (int i = 0; i < num_threads; i++) {
-        pthread_join(threads[i], NULL);
+    // Gather results to rank 0
+    if (rank == 0) {
+        int *global_max = calloc(total_lines, sizeof(int));
+        if (global_max == NULL) {
+            fprintf(stderr, "Rank 0: calloc failed for global_max\n");
+            destroy_fsa(&fsa);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+        for (size_t i = 0; i < my_count; i++) {
+            global_max[my_start + i] = local_max[i];
+        }
+
+        for (int r = 1; r < size; r++) {
+            size_t start_r = 0;
+            for (int i = 0; i < r; i++) {
+                start_r += lines_per_rank + (i < (int)remainder ? 1 : 0);
+            }
+            size_t count_r = lines_per_rank + (r < (int)remainder ? 1 : 0);
+
+            MPI_Recv(global_max + start_r, count_r, MPI_INT, r, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        printf("First 100 lines of output:\n");
+        for (size_t i = 0; i < 100 && i < total_lines; i++) {
+            printf("%zu: %d\n", i, global_max[i]);
+        }
+
+        free(global_max);
+    } 
+    else {
+        MPI_Send(local_max, my_count, MPI_INT, 0, 2, MPI_COMM_WORLD);
     }
 
-    // Print final output
-    for (size_t i = 0; i < my_line_count; i++) {
-        size_t global_line = node_start + i;
-        printf("%zu: %d\n", global_line, max_values[i]);
-    }
-
+    free(local_max);
     destroy_fsa(&fsa);
-    free(max_values);
-    free(threads);
-    free(tdata);
 
     MPI_Finalize();
-
     return 0;
 }
